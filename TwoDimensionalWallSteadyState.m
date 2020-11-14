a0deb4d44120590445ec18d7ba1f4eaa14585229
Darius Mensah <dariusmensah@Dariuss-MBP.fios-router.home>
2020-11-14 01:14:55 -0500

    Updated the loop more
    Still have some bugs tho. Will continue working

--- TwoDimensionalWallSteadyState.m
+++ TwoDimensionalWallSteadyState.m
@@ -12,10 +12,10 @@ Tinf4 = Tinf2;
 L = cm_to_m(1); % 1cm to m 
 H = cm_to_m(30); % 30cm to m 
 iter = 0; % iteration counter
-iterLimit = 10;
+iterLimit = 200;
 
 %% Nodes (horizontal & vertical)
-dimension = [5 5]; % any # of nodes (x-direction) & nodes (y-direction)
+dimension = [10 5]; % any # of nodes (x-direction) & nodes (y-direction)
 
 xNodes = dimension(1); % Across
 dx = L/(xNodes - 1);
@@ -27,42 +27,61 @@ T = zeros(xNodes,yNodes).'; % 'old' temps
 
 temps = zeros(xNodes,yNodes).'; % 'new' temps
 
-%% 2D Boundaries 
-T(1,1:xNodes) = 0;  % Top
-T(end,1:xNodes) = 0; % Bottom
-T(1:yNodes,1) = 0; % Left
-T(1:yNodes,end) = 0; % right
-
 
 while iter < iterLimit
     
     %% Corners 
     
     % Upper Left Corner                                          
-    temps(1,1) = ((h1*dx*(dy^2)*Tinf1) + (k*(dy^2)*T(1,2)) + (k*(dx^2)*T(2,1)) + (h3*(dx^2)*dy*Tinf3)+...
+    temps(1,1) = ((h1*dx*(dy^2)*Tinf1) + (k*(dy^2)*T(1,2)) +...
+        (k*(dx^2)*T(2,1)) + (h3*(dx^2)*dy*Tinf3)+...
         ((egen/2)*dx^2*dy^2))/((h1*dx*(dy^2)) + k*dy^2 + k*dx^2 + (h3*(dx^2)*dy));
 
     % Upper Right Corner                          
-    temps(1,end) = ((h3*dy*(dx^2)*Tinf3) + (k*(dy^2)*T(1,end-1)) + (k*(dx^2)*T(2,end)) + (h2*(dy^2)*dx*Tinf2)+...
+    temps(1,end) = ((h3*dy*(dx^2)*Tinf3) + (k*(dy^2)*T(1,end-1)) +...
+        (k*(dx^2)*T(2,end)) + (h2*(dy^2)*dx*Tinf2)+...
         ((egen/2)*dx^2*dy^2))/((h3*dy*(dx^2)) + k*dx^2 + k*dy^2 + (h2*(dy^2)*dx));
 
     % Lower Left Corner
-    temps(end,1) = ((h1*dx*(dy^2)*Tinf1) + (k*(dy^2)*T(end,2)) + (k*(dx^2)*T(end-1,1)) + (h4*(dx^2)*dy*Tinf4)+...
+    temps(end,1) = ((h1*dx*(dy^2)*Tinf1) + (k*(dy^2)*T(end,2)) +...
+        (k*(dx^2)*T(end-1,1)) + (h4*(dx^2)*dy*Tinf4)+...
         ((egen/2)*dx^2*dy^2))/((h1*dx*(dy^2)) + k*dx^2 + k*dy^2 + (h4*(dx^2)*dy));
 
     % Lower Right Corner
-    temps(end,end) = ((h4*dy*(dx^2)*Tinf4) + (k*(dy^2)*T(end,end-1)) + (k*(dx^2)*T(end-1,end)) + (h2*(dy^2)*dx*Tinf2)+...
+    temps(end,end) = ((h4*dy*(dx^2)*Tinf4) + (k*(dy^2)*T(end,end-1)) +...
+        (k*(dx^2)*T(end-1,end)) + (h2*(dy^2)*dx*Tinf2)+...
         ((egen/2)*dx^2*dy^2))/((h4*dy*(dx^2)) + k*dx^2 + k*dy^2 + (h2*(dy^2)*dx));
 
     %% Sides
-    temps(1,2:xNodes-1) = 1;  % Top
-    temps(end,2:xNodes-1) = 2; % Bottom
-    temps(2:yNodes-1,1) = 3; % Left
-    temps(2:yNodes-1,end) = 4; % right
+    for m = 2:yNodes-1 
+        for n = 2:xNodes-1 
+            
+        % Top
+        temps(1,2:xNodes-1) = ((h3*(dx^2)*dy*Tinf3) + (2*k*(dy^2)*(T(m-1,n) + T(m+1,n))) +...
+            (2*k*(dx^2)*T(m,n+1)) + (egen*(dx^2)*(dy^2)))/((h3*(dx^2)*dy) + (4*k*(dy^2)) +...
+            (2*k*(dx^2)));
+
+        % Bottom
+        temps(end,2:xNodes-1) = ((h4*(dx^2)*dy*Tinf4) + (2*k*(dy^2)*(T(m-1,n) + T(m+1,n))) +...
+            (2*k*(dx^2)*T(m,n-1)) + (egen*(dx^2)*(dy^2)))/((h4*(dx^2)*dy) + (4*k*(dy^2)) +...
+            (2*k*(dx^2)));
+
+
+        % Left
+        temps(2:yNodes-1,1) = ((h1*(dy^2)*dx*Tinf1) + (2*k*(dx^2)*(T(m,n-1) + T(m,n+1))) +...
+            (2*k*(dy^2)*T(m+1,n)) + (egen*(dx^2)*(dy^2)))/((h1*(dy^2)*dx) + (4*k*(dx^2)) +...
+            (2*k*(dy^2)));
+
+        % Right
+        temps(2:yNodes-1,end) = ((h2*(dy^2)*dx*Tinf2) + (2*k*(dx^2)*(T(m,n-1) + T(m,n+1))) +...
+            (2*k*(dy^2)*T(m-1,n)) + (egen*(dx^2)*(dy^2)))/((h2*(dy^2)*dx) + (4*k*(dx^2)) +...
+            (2*k*(dy^2)));
+        end 
+    end 
     
     %% Interior nodes    
-    for i = 2:yNodes-1 % rows
-        for j = 2:xNodes-1 % colmuns     
+    for i = 2:yNodes-1
+        for j = 2:xNodes-1 
         temps(i,j) = ((k*dy^2*(T(i+1,j)+T(i-1,j))) + (k*dx^2*(T(i,j+1)+T(i,j-1))) +...
             (egen*(dx^2)*(dy^2)))/(k*(2*dy^2 +2*dx^2));
         end 
