5b936aa2fde5170327f377cdad5343aa5d9afdae
Darius Mensah <dariusmensah@Dariuss-MacBook-Pro.local>
2020-11-20 13:34:28 -0500

    Added a scalable node system to the coolant channel

--- CoolantChannelDarius.m
+++ CoolantChannelDarius.m
@@ -1,66 +1,71 @@
 %% Adding the functions to the filepath
 addpath('ProjectFunctions')
 
-A = zeros(10,5);
+A = zeros(50,25);
 b = size(A);
-xdim = b(2);
-ydim = b(1);
 
-rows = ydim;
-cols = xdim;
+%% Rows
+TopRecArea = Percent(30); % 30% of the total area
+TopRec =  ceil(b(1)*TopRecArea); % 30% of 50
 
+BottomRecArea = Percent(15); % 15% of the total area
+BottomRec = ceil(b(1)*BottomRecArea); % 15% of 50 (rounded up)
 
-% Bottom Rectangle
-BottomRecX = cm_to_m(0.20); % 0.20cm to m 
-BottomRecY = cm_to_m(0.05); % 0.05cm to m 
-        
-BottomRec_dx = BottomRecX/(xdim-1);
-BottomRec_dy = BottomRecY/(ydim-1);
+Remaining = 1-(TopRecArea+BottomRecArea);
+CenterRec = floor(b(1)*Remaining); % 55% of 50
 
-% Left Rectangle
-LeftRecX = cm_to_m(0.05); % 0.05cm to m 
-LeftRecY = cm_to_m(0.35); % 0.35cm to m 
+CoolantArea = floor((TopRec+CenterRec));
+%% Columns
+LeftRecAcross = Percent(20); % the left rectangle makes up 20 of the cols
+LeftRec = b(2)*LeftRecAcross;
+RemainingAcross = 1-LeftRecAcross;
+RemainingCols = b(2)*RemainingAcross;
 
-LeftRec_dx = LeftRecX/(xdim-1);
-LeftRec_dy = LeftRecY/(ydim-1);
+rows = b(1);
+cols = b(2);
 
-% Top Rectangle
-TopRecX = cm_to_m(0.20); % 0.20cm to m 
-TopRecY = cm_to_m(0.1); % 0.1cm to m 
-
-TopRec_dx = TopRecX/(xdim-1);
-TopRec_dy = TopRecY/(ydim-1);
-
-% Top Rectangle
-for i = 1:2
-    for j = 2:cols
-        A(i,j) = 10;
+%% Top Rectangle
+for i = 1:TopRec
+    for j = LeftRec+1:cols
+        A(i,j) = 55;
     end 
 end 
 
-% Left Rectangle
-for i = 2:rows-1
-    for j = 1
-        A(i,j) = 22;
+%% Left Rectangle
+for i = 1:rows
+    for j = 1:LeftRec
+        A(i,j) = 23;
     end 
 end 
 
-% Bottom Rectangle
-for i = ydim
-    for j = 2:cols-1
-        A(i,j) = 70;
-    end 
-end 
-      
-% Interior 
-for i = 3:rows-1
-    for j = 2:cols
-        A(i,j)= 183;
+%% Center Rec (Coolant Channel)
+for i = TopRec:CoolantArea
+    for j = LeftRec+1:cols
+        A(i,j) = 183;
     end 
 end 
 
-% Corners
-A(1,1) = 5;
-A(1,end) = 5;
-A(end,1) = 5;
-A(end,end) = 5;
+
+%% Bottom Rec 
+for i = CoolantArea:(CoolantArea+BottomRec)
+    for j = LeftRec+1:cols
+        A(i,j) = 97;
+    end 
+end 
+
+
+%% Outer Corners
+A(1,1) = 987;
+A(1,end) = 987;
+A(end,1) = 987;
+A(end,end) = 987;
+
+%% Inner Corners
+A(TopRec,LeftRec+1) = 855;
+A(TopRec,end) = 855;
+A(CoolantArea-1,LeftRec+1) = 855;
+A(CoolantArea-1,end) = 855;
+
+function A = Percent(B)
+A = B/100;
+end 
